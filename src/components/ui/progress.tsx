0792242efa5d57c43b2d8cc9f9065d884dee4041
Heynan7 <melvingomez1000@gmail.com>
2025-10-10 14:49:01 -0600

    ultimos cambios

---
+++ src/components/ui/progress.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+export interface ProgressProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  value?: number
+}
+
+export function Progress({ value = 0, className, ...props }: ProgressProps) {
+  return (
+    <div
+      className={cn(
+        "relative h-2 w-full overflow-hidden rounded-full bg-muted",
+        className
+      )}
+      {...props}
+    >
+      <div
+        className="h-full w-full flex-1 bg-sky-500 transition-all duration-300 ease-in-out"
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </div>
+  )
+}
