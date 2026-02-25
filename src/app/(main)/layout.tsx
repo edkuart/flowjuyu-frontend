0bd0fb6da9d582e31a4e0a8dbff8d360b68a5f70
Eku-sama <edkuart@gmail.com>
2026-02-24 19:28:17 -0600

    fix: remove duplicated Header from (main) layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,4 @@
 import { AppSidebar } from "@/components/ui/sidebar/AppSidebar";
-import Header from "@/components/layout/Header";
 
 export default function MainLayout({
   children,
@@ -11,10 +10,6 @@ export default function MainLayout({
       <AppSidebar />
 
       <div className="flex-1 flex flex-col">
-        <header className="sticky top-0 z-50 bg-white shadow md:px-6 px-3 py-2">
-          <Header />
-        </header>
-
         <main className="flex-1 px-3 md:px-6 py-4 md:py-6">
           {children}
         </main>
